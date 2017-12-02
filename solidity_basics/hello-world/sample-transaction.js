c882de36150bbb5abfdf0b97baf137b2c4ee68d5
--global <borislavborisov22@abv.bg>
2017-12-03 01:10:51 +0200

    Added sample transaction from one account to another  on the testrpc with web3.js

---
+++ solidity_basics/hello-world/sample-transaction.js
@@ -0,0 +1,22 @@
+const Web3 = require('web3');
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+const getBalanceInEther = (web3, account) => {
+    if (!(web3 && account)) {
+        throw new Error('Both web3Object and account must be provided to function');
+    }
+
+    const balanceInEther = web3.fromWei(web3.eth.getBalance(account), 'ether').toNumber();
+    return balanceInEther;
+};
+
+const aliceAccount = web3.eth.accounts[0];
+const bobAccount = web3.eth.accounts[1];
+
+
+// transaction from alice to bob :)
+web3.eth.sendTransaction({ from: aliceAccount, to: bobAccount, value: web3.toWei(10, 'ether') });
+
+console.log(getBalanceInEther(web3, aliceAccount));
+console.log(getBalanceInEther(web3, bobAccount));
