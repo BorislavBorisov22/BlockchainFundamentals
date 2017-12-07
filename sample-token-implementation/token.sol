bb314ff25a78cbb79c87a87e623a3ebd1a3ad5a1
--global <borislavborisov22@abv.bg>
2017-12-08 00:35:11 +0200

    Added token imporvementes like buying and selling token in exchange of ether and owner functionality like minting new tokens

--- sample-token-implementation/token.sol
+++ sample-token-implementation/token.sol
@@ -4,7 +4,25 @@ interface tokenRecipient {
     function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public;
 }
 
-contract TokenERC20 {
+contract Owned {
+    
+    address owner;
+    
+    function Owned() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function transferOwnership(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+    }
+}
+
+contract TokenERC20 is Owned{
     
     string public name;
     string public symbol;
@@ -12,22 +30,29 @@ contract TokenERC20 {
     
     uint256 public totalSupply;
     
-    address public owner;
+    uint256 public buyPrice;
+    uint256 public sellPrice;
+    uint public minBalanceForAccounts;
     
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping(address => uint256)) public allowance;
+    mapping (address => bool) public frozen;
     
     event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    
+    event FrozenFunds(address indexed _fronzenAddress);
     event Burn(address indexed _from, uint256 _amount);
     
-    function TokenERC20(string _name, string _symbol, uint256 _initialSupply) public {
+    function TokenERC20(string _name, string _symbol, uint256 _initialSupply, address _centralMinter) public {
         name = _name;
         symbol = _symbol;
         totalSupply = _initialSupply * 10 ** uint256(decimals);
         
         owner = msg.sender;
-        balanceOf[msg.sender] += _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        
+        if (_centralMinter != 0x0){
+            owner = _centralMinter;
+        }
     }
     
     function _transfer(address _from, address _to, uint256 _amount) internal {
@@ -50,7 +75,12 @@ contract TokenERC20 {
     }
     
     function transfer(address _to, uint256 _amount) public {
+        require(!frozen[msg.sender]);
         _transfer(msg.sender, _to, _amount);   
+        
+        if (balanceOf[msg.sender] < minBalanceForAccounts) {
+            balanceOf[msg.sender] = minBalanceForAccounts;
+        }
     }
     
     function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
@@ -102,5 +132,54 @@ contract TokenERC20 {
         Burn(_from, _value);
         
         return true;
+    }
+    
+    function mintToken(address _target, uint256 _mintedAmout) public {
+        balanceOf[_target] += _mintedAmout;
+        totalSupply += _mintedAmout;
+        
+        Transfer(0, owner, _mintedAmout);
+        Transfer(owner, _target, _mintedAmout);
+    }
+    
+    function freezeAccount(address _target) public onlyOwner {
+        frozen[_target] = true;
+        FrozenFunds(_target);
+    }
+    
+    function setPrice(uint256 _buyPrice, uint256 _sellPrice) public onlyOwner {
+        buyPrice = _buyPrice;
+        sellPrice = _sellPrice;
+    }
+    
+    function buy() public payable returns (uint256) {
+        uint256 amount = msg.value / buyPrice;
+        
+        require(balanceOf[this] >= amount);
+        balanceOf[msg.sender] += amount;
+        balanceOf[this] -= amount;
+        
+        Transfer(this, msg.sender, amount);
+        return amount;
+    }
+    
+    function sell(uint256 _amount) public returns (uint256 revenue) {
+        require(balanceOf[msg.sender] >= _amount);
+        
+        _transfer(msg.sender, this, _amount);
+        
+        revenue = _amount * sellPrice;
+        require(msg.sender.send(revenue));
+        
+        Transfer(msg.sender, this, _amount);
+        return revenue;
+    }
+    
+    function setMinBalanceForAccounts(uint minBalanceInFinney) public onlyOwner {
+        minBalanceForAccounts = minBalanceInFinney * 1 finney;
+    }
+    
+    function giveReward() public {
+        balanceOf[block.coinbase] += 1;
     }
 }
